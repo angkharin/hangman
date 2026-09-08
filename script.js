89a3590bb08bb4f912b55f2898f18cdec5be2f5f
angkharin <angsae20@gmail.com>
2026-09-08 22:43:43 +0700

    ีีud

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
+// Firebase Configuration
 const firebaseConfig = {
-
-    apiKey: "ใส่_API_KEY_ของคุณ",
-
-    authDomain: "ใส่_PROJECT_ID.firebaseapp.com",
-
-    projectId: "ใส่_PROJECT_ID",
-
-    storageBucket: "ใส่_PROJECT_ID.firebasestorage.app",
-
-    messagingSenderId: "ใส่_MESSAGING_SENDER_ID",
-
-    appId: "ใส่_APP_ID"
-
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
