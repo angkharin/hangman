7d05619a4969740b9ec2abf56437e0fd1a525c60
angkharin <angsae20@gmail.com>
2026-09-08 22:48:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "ค่าจริงของคุณ",
-  authDomain: "ชื่อโปรเจกต์จริง.firebaseapp.com",
-  projectId: "ชื่อโปรเจกต์จริง",
-  storageBucket: "ค่าจริง",
-  messagingSenderId: "ค่าจริง",
-  appId: "ค่าจริง"
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "..."
 };
